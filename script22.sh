cf18811bd162c3134a755a4b2f3ca34bfa1289c7
Sujal Pakhale <152052789+SujalPakhale@users.noreply.github.com>
2025-04-23 14:24:41 +0530

    script22.sh

--- script22.sh
+++ script22.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Log Rotation
-log_dir="/var/log"
-find "$log_dir" -name "*.log" -mtime +7 -exec gzip {} \;
-echo "Old logs compressed."
+echo "Enter a number:"
+read num
+sum=0
+while [ $num -gt 0 ]; do
+    digit=$((num % 10))
+    sum=$((sum + digit))
+    num=$((num / 10))
+done
+echo "Sum of digits: $sum"
