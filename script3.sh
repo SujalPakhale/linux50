9aeea24d23be872659bc7d9c0906299ec4a5a566
Sujal Pakhale <152052789+SujalPakhale@users.noreply.github.com>
2025-04-23 14:27:10 +0530

    script3.sh

--- script3.sh
+++ script3.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Find and Delete Old Files
-read -p "Enter path: " path
-read -p "Delete files older than (days): " days
-if [ ! -d "$path" ]; then
-    echo "Error: Invalid directory!"
-    exit 1
+echo "Enter a year:"
+read year
+if [ $((year % 4)) -eq 0 ] && [ $((year % 100)) -ne 0 ] || [ $((year % 400)) -eq 0 ]; then
+    echo "Leap Year"
+else
+    echo "Not a Leap Year"
 fi
-find "$path" -type f -mtime +"$days" -exec rm -v {} \;
-echo "Deleted files older than $days days."
