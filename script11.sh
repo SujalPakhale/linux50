8f7ffa6d7fe397162beae78e47f350545690ec16
Sujal Pakhale <152052789+SujalPakhale@users.noreply.github.com>
2025-04-23 14:18:35 +0530

    script11.sh

--- script11.sh
+++ script11.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-# Automated Git Backup
-repo_path="/path/to/your/repo"
-commit_msg="Auto-commit: $(date +'%Y-%m-%d %H:%M:%S')"
-cd "$repo_path" || exit 1
-if [ -n "$(git status --porcelain)" ]; then
-    git add .
-    git commit -m "$commit_msg"
-    git push origin main
-    echo "Changes pushed to Git."
+echo "Enter a number:"
+read num
+if [[ "$num" == "$(echo $num | rev)" ]]; then
+    echo "Palindrome"
 else
-    echo "No changes detected."
+    echo "Not a Palindrome"
 fi
