86c40e2eb8118fdb32c12c4f5af1e95099b6e278
Sujal Pakhale <152052789+SujalPakhale@users.noreply.github.com>
2025-04-23 14:33:34 +0530

    script7.sh

--- script7.sh
+++ script7.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Disk Space Alert
-threshold=90
-usage=$(df / | awk '/\// {print $5}' | tr -d '%')
-if [ "$usage" -ge "$threshold" ]; then
-    echo "Warning: Disk usage is $usage%!" | mail -s "Disk Alert" admin@example.com
-    echo "Alert sent!"
-else
-    echo "Disk usage is OK ($usage%)."
+echo "Enter filename:"
+read filename
+if [ -r "$filename" ]; then
+    echo "Readable"
+fi
+if [ -w "$filename" ]; then
+    echo "Writable"
+fi
+if [ -x "$filename" ]; then
+    echo "Executable"
 fi
