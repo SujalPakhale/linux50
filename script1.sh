9604f61b2eea4fa8ff06d30e0e694b37a559ca90
Sujal Pakhale <152052789+SujalPakhale@users.noreply.github.com>
2025-04-23 14:16:33 +0530

    script1.sh

--- script1.sh
+++ script1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# System Monitoring Dashboard
-echo -e "\n===== SYSTEM MONITOR ====="
-echo "CPU Usage: $(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')%"
-echo "Memory Usage: $(free -m | awk '/Mem:/ {print $3 "MB / " $2 "MB"}')"
-echo "Disk Usage: $(df -h / | awk '/\// {print $5 " used (" $3 " / " $2 ")"}')"
-echo "Network: $(ip a | grep "inet " | grep -v "127.0.0.1")"
-echo -e "=========================\n"
+echo "Enter a string:"
+read str
+if [[ "$str" == "$(rev <<< "$str")" ]]; then
+    echo "Palindrome"
+else
+    echo "Not a Palindrome"
+fi
