7506a56e5035707a6d18b5df9d0ad4369484fe02
Sujal Pakhale <152052789+SujalPakhale@users.noreply.github.com>
2025-04-23 14:23:33 +0530

    script19.sh

--- script19.sh
+++ script19.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-# CSV to JSON Converter
-read -p "Enter CSV file: " csvfile
-if [ ! -f "$csvfile" ]; then
-    echo "Error: File not found!"
-    exit 1
+echo "Enter a number:"
+read num
+is_prime=1
+for ((i=2; i*i<=num; i++)); do
+    if [ $((num % i)) -eq 0 ]; then
+        is_prime=0
+        break
+    fi
+done
+if [ $num -le 1 ]; then
+    is_prime=0
 fi
-jsonfile="${csvfile%.*}.json"
-awk -F, 'NR==1 {split($0, headers)} NR>1 {for (i=1; i<=NF; i++) printf "\"%s\":\"%s\"%s", headers[i], $i, (i<NF?", ":"")}' "$csvfile" | jq -s .
-echo "JSON saved as $jsonfile"
+if [ $is_prime -eq 1 ]; then
+    echo "Prime"
+else
+    echo "Not Prime"
+fi
